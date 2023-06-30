b6797a729e198d88fd1ae13790efdad5581017f3
annzhzho@gmail.com <annzhzho@gmail.com>
2023-06-30 19:58:45 +0300

    add validation function

---
+++ registration form/index.js
@@ -0,0 +1,45 @@
+const registerForm = document.forms.registration;
+const formElements = registerForm.elements;
+const formName = formElements.name;
+const formEmail = formElements.email;
+const formAge = formElements.age;
+const formProfession = formElements.profession;
+const formPassword = formElements.password;
+const formPasswordRepeat = formElements.passwordRepeat;
+const formAgreement = formElements.agreement;
+
+const errorMessage = document.querySelector(".error");
+
+formEmail.oninput = function () {
+  const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const errorEmail = registerForm.querySelector(".error-email");
+  if (formEmail.value.match(mailFormat)) {
+    errorEmail.textContent = "";
+    return true;
+  } else {
+    errorEmail.textContent = "Пожалуйста, введите корректный email";
+    return false;
+  }
+};
+
+//валидация формы
+function validation(form, inputs, error) {
+  let result = true;
+  for (let input of inputs) {
+    if (input.value === "") {
+      return (error.textContent = `Пожалуйста, заполните все обязательные поля`);
+      result = false;
+    }
+  }
+  error.textContent = ``;
+  return result;
+}
+
+//отправка формы
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (validation(this, formElements, errorMessage) === true) {
+    console.log(`Регистрация завершена`);
+    this.reset();
+  }
+});
